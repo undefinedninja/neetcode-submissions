d69cc5174ef7aa06e5cb8e2dd34f77cd330aebe3
undefinedninja <103439317+undefinedninja@users.noreply.github.com>
2026-08-21 23:44:35 +0530

    Add: counting-bits - submission-3

---
+++ Data Structures & Algorithms/counting-bits/submission-3.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public int[] CountBits(int n) {
+        int[] dp = new int[n+1];
+        for(int i = 0; i <= n ; i++)
+        {
+            dp[i] = dp[i >> 1] + (i & 1);
+        }
+        return dp;
+    }
+}
