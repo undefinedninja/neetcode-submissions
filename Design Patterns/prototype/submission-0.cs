efd9eb7b1deba6816ad91651314c8f92b07f99ff
undefinedninja <103439317+undefinedninja@users.noreply.github.com>
2026-09-02 11:33:05 +0530

    Add: prototype - submission-0

---
+++ Design Patterns/prototype/submission-0.cs
@@ -0,0 +1,52 @@
+public interface IShape {
+    IShape Clone();
+}
+
+public class Rectangle : IShape {
+    private int width;
+    private int height;
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int GetWidth() {
+        return this.width;
+    }
+
+    public int GetHeight() {
+        return this.height;
+    }
+
+    public IShape Clone() {
+        return new Rectangle(this.width, this.height);
+    }
+}
+
+public class Square : IShape {
+    private int length;
+
+    public Square(int length) {
+        this.length = length;
+    }
+
+    public int GetLength() {
+        return this.length;
+    }
+
+    public IShape Clone() {
+        return new Square(this.length);
+    }
+}
+
+public class Test {
+    public List<IShape> CloneShapes(List<IShape> shapes) {
+        List<IShape> sh = new List<IShape>();
+        foreach(IShape s in shapes)
+        {
+            sh.Add(s.Clone());
+        }
+        return sh;
+    }
+}
